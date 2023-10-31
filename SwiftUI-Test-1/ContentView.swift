7b1c97bf852b35e446d2d5408fc23aba46f22039
junjie wu <junjie.wu01@liulishuo.com>
2023-10-31 11:05:30 +0800

    Initial Commit

---
+++ SwiftUI-Test-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Test-1
+//
+//  Created by junjie wu on 2023/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Test-1/SwiftUI_Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Test_1App.swift
+//  SwiftUI-Test-1
+//
+//  Created by junjie wu on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
